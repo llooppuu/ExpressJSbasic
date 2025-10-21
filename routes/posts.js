99bbff71f86a9713d2f5d2aef5b059809a6e64ef
lauri <lauri.vosmi@voco.ee>
2025-10-21 09:18:45 +0300

    added routes folder

---
+++ routes/posts.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const { getPosts, createPost, getPostById, updatePost, publishPost, deletePost } =
+require('../controllers/posts')
+
+router.get('/', (req, res) => getPosts(req, res));
+
+router.post('/', (req, res) => createPost(req, res));
+
+router.get('/:id', (req, res) => getPostById(req, res));
+
+router.put('/:id', (req, res) => updatePost(req, res));
+
+router.patch('/:id/publish', (req, res) => publishPost(req, res));
+
+router.delete('/:id', (req, res) => deletePost(req, res));
+
+module.exports = router
