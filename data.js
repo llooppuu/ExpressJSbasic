5e3bc224895b7e49776181b9c5a8a895401e9099
lauri <lauri.vosmi@voco.ee>
2025-10-24 13:00:09 +0300

    Added utils and turned into json

--- data.js
+++ data.js
@@ -1,20 +1,47 @@
-const posts = [
-    { 
-        id: 1, 
-        title: 'First Post', 
-        content: 'This is the first post.',
-        author: 'First Author',
-        published: true,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-    },
-    { 
-        id: 2, 
-        title: 'Second Post', 
-        content: 'This is the second post.',
-        author: 'First Author',
-        published: false,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString()
-    }
-];
+[
+  {
+    "id": 1,
+    "title": "asdddd",
+    "content": "dfgdhsfgh",
+    "author": "First author",
+    "published": true,
+    "createdAt": "2025-10-23T20:19:16.758Z",
+    "updatedAt": "2025-10-23T20:19:16.758Z"
+  },
+  {
+    "id": 2,
+    "title": "kkk55",
+    "content": "9ipirkthporkjoithjro",
+    "author": "Second author",
+    "published": true,
+    "createdAt": "2025-10-23T20:21:45.758Z",
+    "updatedAt": "2025-10-23T20:23:11.113Z"
+  },
+  {
+    "title": "testttt",
+    "content": "testestetste",
+    "author": "Third author",
+    "published": false,
+    "id": 3,
+    "createdAt": "2025-10-23T20:55:10.311Z",
+    "updatedAt": "2025-10-23T20:57:10.311Z"
+  },
+  {
+    "title": "tttttt",
+    "content": "ttttttttttttttttttttttt",
+    "author": "Fourth author",
+    "published": false,
+    "id": 4,
+    "createdAt": "2025-10-23T21:05:17.255Z",
+    "updatedAt": "2025-10-23T21:06:19.255Z"
+  },
+  {
+    "title": "aaadasfdsf",
+    "content": "aaaaaaabbbbbbbbbbccccccccdddddddd",
+    "author": "Fifth author",
+    "published": false,
+    "id": 5,
+    "createdAt": "2025-10-23T21:15:23.022Z",
+    "updatedAt": "2025-10-23T21:16:45.023Z"
+  }
+]
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { getPosts, createPost, getPostById, updatePost, publishPost, deletePost } =
-require('../controllers/posts')
+const { getPosts, postPost, getPostByID, putPost, patchPost, deletePost } = require('../controllers/posts');
 
-router.get('/', (req, res) => getPosts(req, res));
+router.get('/', getPosts);
+router.post('/', postPost);
+router.get('/:id', getPostByID);
+router.put('/:id', putPost);
+router.patch('/:id/publish', patchPost);
+router.delete('/:id', deletePost);
 
-router.post('/', (req, res) => createPost(req, res));
-
-router.get('/:id', (req, res) => getPostById(req, res));
-
-router.put('/:id', (req, res) => updatePost(req, res));
-
-router.patch('/:id/publish', (req, res) => publishPost(req, res));
-
-router.delete('/:id', (req, res) => deletePost(req, res));
-
-module.exports = router
+module.exports = router;
